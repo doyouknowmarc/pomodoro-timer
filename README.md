64add65cbda3d686c29072b8b83547c91a115b67
doyouknowmarc <doyouknowmarc@gmail.com>
2025-01-04 12:54:42 +0100

    adjusted the path and added the missing tick file

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 
 ## todo:
 - favicon
-- tabtitle
-- audio
 
 [Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/doyouknowmarc/pomodoro-timer)
--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/pomodoro-timer/",
+  base: '/pomodoro-timer/',
+  build: {
+    assetsInclude: ['**/*.mp3']
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name.endsWith('.mp3')) {
-            return 'assets/audio/[name]-[hash][extname]'
-          }
-          return 'assets/[name]-[hash][extname]'
-        }
-      }
-    }
   },
 });
